31269ed465a6d9c83de286a2e678a8de0195a428
alexdoffgit <102202968+alexdoffgit@users.noreply.github.com>
2022-05-14 10:45:58 +0700

    implementation of absen

---
+++ backend/adapter/prismapg/absen.ts
@@ -0,0 +1,30 @@
+import { PRISMA } from "./singleton";
+import { IAbsen, Absen as AbsenContract } from "@model/absen";
+import { nanoid } from "nanoid";
+
+
+export class Absensi implements IAbsen {
+    public async masuk(absen: AbsenContract) {
+        const id = nanoid()
+        await PRISMA.absen.create({
+            data: {
+                id: id,
+                hari: absen.hari,
+                tipe: absen.tipe,
+                jamMulai: absen.jamMulai,
+                keterangan: absen.keterangan
+            }
+        });
+    }
+
+    public async keluar(absen: AbsenContract) {
+        await PRISMA.absen.update({
+            where: {
+                hari: absen.hari
+            },
+            data: {
+                jamSelesai: absen.jamSelesai
+            }
+        })
+    }
+}
