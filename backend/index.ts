8cbc67eb02962a605987be0deb514978e2f08152
alexdoffgit <102202968+alexdoffgit@users.noreply.github.com>
2022-05-14 10:47:35 +0700

    add masuk endpoint to router

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,15 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import dotnev from "dotenv";
+import { masuk } from "./endpoint/absen/masuk";
+import { Absensi } from "@prismapg/absen";
 
 dotnev.config()
 
 const app: Application = express();
 
-app.use('/', (req: Request, res: Response) => {
-    res.status(200).send("hello aaa")
-})
+app.use(express.json())
+
+const absenAdapter = new Absensi()
+app.post("/absen/masuk", masuk(absenAdapter))
 
 app.listen(process.env.APP_PORT, () => console.log(`Server listening on ${process.env.APP_PORT}`))
