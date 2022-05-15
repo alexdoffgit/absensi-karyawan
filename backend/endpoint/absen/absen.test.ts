9887dd6e46b7680cfd864906a2f5596fb40faa42
alexdoffgit <102202968+alexdoffgit@users.noreply.github.com>
2022-05-15 16:43:54 +0700

    unified all absen endpoint test

--- backend/endpoint/absen/absen.test.ts
+++ backend/endpoint/absen/absen.test.ts
@@ -1,10 +1,10 @@
 import { absenMasukAdapter, masuk } from "./masuk"
-import { AbsenMasukReq } from "../../domain/dto/absen";
+import { AbsenKeluarReq, AbsenMasukReq } from "../../domain/dto/absen";
 import { Absen } from "../../domain/model/absen";
-import { nanoid } from "nanoid";
 import { addHours } from "date-fns";
 import { Absensi } from "../../adapter/mock/absen";
 import { createRequest, createResponse } from "node-mocks-http";
+import { absenKeluarAdapter, keluar } from "./keluar";
 
 test("test apakah absen dto menjadi absen masuk", () => {
     const gmt = new Date()
@@ -31,7 +31,7 @@ test("test apakah absen dto menjadi absen masuk", () => {
     expect(absenMasukAdapter(req)).toEqual(absen)
 })
 
-test("test router masuk", async () => {
+test("test router masuk", () => {
     const gmt = new Date()
     const iso = new Date(gmt.toISOString())
     const jamMulai = addHours(iso, 2)
@@ -52,6 +52,53 @@ test("test router masuk", async () => {
     const a = new Absensi()
 
     masuk(a)(req, res)
+
+    expect(res._getStatusCode()).toEqual(200)
+})
+
+test("parse absen keluar dto into absen object", () => {
+    const gmt = new Date()
+    const iso = new Date(gmt.toISOString())
+    
+    const req: AbsenKeluarReq = {
+        hari: iso.toString(),
+        jamSelesai: iso.toString(),
+        tipe: "absen"
+    }
+
+    const keluar: Absen = {
+        id: "",
+        hari: new Date(iso.toString()),
+        jamMulai: null,
+        jamSelesai: new Date(iso.toString()),
+        keterangan: null,
+        lampiran: null,
+        tipe: "absen",
+    }
+
+    expect(absenKeluarAdapter(req)).toEqual(keluar)
+})
+
+test("test absen keluar endpoint", () => {
+    const gmt = new Date()
+    const iso = new Date(gmt.toISOString())
+    
+    const payload: AbsenKeluarReq = {
+        hari: iso.toString(),
+        jamSelesai: addHours(iso, 2).toString(),
+        tipe: "absen"
+    }
+
+    let req = createRequest({
+        method: "POST",
+        url: "/absen/keluar",
+        body: payload
+    })
+
+    let res = createResponse()
+
+    const mock = new Absensi()
+    keluar(mock)(req, res)
 
     expect(res._getStatusCode()).toEqual(200)
 })
